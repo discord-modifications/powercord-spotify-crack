43bad18a16e9c59103afee299cdda5f8d20a9ec3
eternal <eternal.git@gmail.com>
2022-08-11 19:16:43 +0100

    cleanup

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const { FluxDispatcher: Dispatcher, getModule } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 const { Plugin } = require('powercord/entities');
 
-const isPremium = getModule(['isSpotifyPremium'], false);
+const Spotify = getModule(['isSpotifyPremium'], false);
 const Profile = getModule(['getProfile'], false);
 
 module.exports = class extends Plugin {
    startPlugin() {
-      require('powercord/webpack').getModule(['isSpotifyPremium'], false).isSpotifyPremium = () => true
-      inject('spotify-crack', Profile, 'getProfile', async function (args) {
-         inject('spotify-crack1', isPremium, 'isSpotifyPremium', async function (_) {
-            return true;
-         });
+      inject('spotify-crack-is', Spotify, 'isSpotifyPremium', () => Promise.resolve(true));
+
+      Spotify._ensureSpotifyPremium = Spotify.ensureSpotifyPremium;
+      Spotify.ensureSpotifyPremium = () => Promise.resolve(true);
+
+      inject('spotify-crack', Profile, 'getProfile', async (args) => {
          Dispatcher.dispatch({ type: 'SPOTIFY_PROFILE_UPDATE', accountId: args[0], isPremium: true });
-         uninject('spotify-crack1');
-         return;
       });
    }
 
    pluginWillUnload() {
       uninject('spotify-crack');
-      uninject('spotify-crack1');
+      uninject('spotify-crack-is');
+      
+      Spotify.ensureSpotifyPremium = Spotify._ensureSpotifyPremium
    }
 };
